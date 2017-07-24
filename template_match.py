bedaeda8395428282c1c3891feda3d862e47ce50
gayatri.behera <gayatri.behera@drivedge.com>
2017-07-24 18:28:23 +0530

    2

--- template_match.py
+++ template_match.py
@@ -1,5 +1,7 @@
 #template matching -
-#additional resource - http://docs.opencv.org/3.0-rc1/d4/dc6/tutorial_py_template_matching.html
+#additional resource - 1. http://docs.opencv.org/3.0-rc1/d4/dc6/tutorial_py_template_matching.html
+# 2. http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_template_matching/py_template_matching.html#exercises
+# 3. https://pythonspot.com/en/object-detection-with-templates/
 
 import numpy as np
 import cv2
@@ -8,9 +10,11 @@ img_rgb = cv2.imread('D:\\SentDex\\images\\big.jpg')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('D:\\SentDex\\images\\small.jpg', 0)
+
+# Store width and heigth of template in w and h
 w,h = template.shape[::-1]
 
-#considering all the template matching parameters
+#considering all the template matching parameters -
 #methods = [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED]
 
 #for method in methods:
@@ -37,6 +41,7 @@ print (maxVal)
 print (minLoc)
 print (maxLoc)
 
+# Draw a rectangle around the matched region.
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0]+w, pt[1]+h), (0, 255, 255), 2)
 
